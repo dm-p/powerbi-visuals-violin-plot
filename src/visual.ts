a9baa2f43b0801e57f1a68509275970ec0865694
Daniel Marsh-Patrick <d.s.marsh@gmail.com>
2018-09-28 14:40:52 +1200

    #2: Removed histogram function, as we want to do the KDE function for the lines

--- src/visual.ts
+++ src/visual.ts
@@ -2137,10 +2137,6 @@ module powerbi.extensibility.visual {
                         quartile1: d3.quantile(dataPoints, 0.25),
                         quartile3: d3.quantile(dataPoints, 0.75),
                         confidenceUpper: d3.quantile(dataPoints, 0.95),
-                        histogram: d3.layout.histogram()
-                                    .bins(20) /** TODO: make resolution configurable? */
-                                    .frequency(false)
-                                    (dataPoints),
                         dataPoints: dataPoints
                     }
                 })
