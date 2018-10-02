bb5c9d1a98175bab20c892015af8f0e81a5857a9
Daniel Marsh-Patrick <d.s.marsh@gmail.com>
2018-10-02 15:01:50 +1300

    #11: Initial view model to ensure that 0.1.0 prototype will work correctly when doing visual transforms. Resolves #11.

---
+++ src/viewModels.ts
@@ -0,0 +1,41 @@
+module powerbi.extensibility.visual {
+
+    import axisHelper = powerbi.extensibility.utils.chart.axis;
+    import IAxisProperties = powerbi.extensibility.utils.chart.axis.IAxisProperties;
+
+    export module ViolinPlotModels {
+
+        export interface IViewModel {
+            categories: ICategory[];
+            statistics: IStatistics;
+            yAxis: axisHelper.IAxisProperties;
+            xVaxis: axisHelper.IAxisProperties;
+        }
+
+        export interface ICategory {
+            name: string;
+            dataPoints: number[];
+            dataKde: IDataPointKde[];
+            statistics: IStatistics;
+            yVScale: d3.scale.Linear<number, number>;
+        }
+
+        export interface IStatistics {
+            min: number;
+            confidenceLower: number;
+            quartile1: number;
+            median: number;
+            mean: number;
+            quartile3: number;
+            confidenceUpper: number;
+            max: number;
+        }
+
+        export interface IDataPointKde {
+            x: number;
+            y: number;
+        }
+
+    }
+
+}
