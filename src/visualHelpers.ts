345179053d2bdc7351190c73bef9187cc7f49dba
Daniel Marsh-Patrick <d.s.marsh@gmail.com>
2018-11-13 12:48:10 +1300

    #9: Added function to get categorical object values (dynamic colours)

--- src/visualHelpers.ts
+++ src/visualHelpers.ts
@@ -54,7 +54,35 @@ module powerbi.extensibility.visual {
             }
         }
 
-        export function visualTransform(options: VisualUpdateOptions, settings: VisualSettings) : IViewModel {
+        /**
+         * Gets property value for a particular object in a category.
+         *
+         * @function
+         * @param {DataViewCategoryColumn} category - List of category objects.
+         * @param {number} index                    - Index of category object.
+         * @param {string} objectName               - Name of desired object.
+         * @param {string} propertyName             - Name of desired property.
+         * @param {T} defaultValue                  - Default value of desired property.
+         */
+            export function getCategoricalObjectValue<T>(category: DataViewCategoryColumn, index: number, objectName: string, propertyName: string, defaultValue: T): T {
+                let categoryObjects = category.objects;
+
+                if (categoryObjects) {
+                    let categoryObject: DataViewObject = categoryObjects[index];
+                    if (categoryObject) {
+                        let object = categoryObject[objectName];
+                        if (object) {
+                            let property: T = <T>object[propertyName];
+                            if (property !== undefined) {
+                                return property;
+                            }
+                        }
+                    }
+                }
+                return defaultValue;
+            }
+
+        export function visualTransform(options: VisualUpdateOptions, settings: VisualSettings, host: IVisualHost, colourPalette: IColorPalette) : IViewModel {
 
             /** Set up debugging */
                 let debug = new VisualDebugger(settings.about.debugMode && settings.about.debugVisualUpdate);
