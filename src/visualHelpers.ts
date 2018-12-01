c828e44447f2e00b88d6b69d7ffd6c1b52405ae9
Daniel Marsh-Patrick <d.s.marsh@gmail.com>
2018-12-02 12:23:05 +1300

    #44: Moved debugging into its own class

--- src/visualHelpers.ts
+++ src/visualHelpers.ts
@@ -29,50 +29,6 @@ module powerbi.extensibility.visual {
             import ELimit = KDE.ELimit;
 
         /**
-         * Used to handle debugging, if enabled within the visual settings
-         */
-            export class VisualDebugger {
-                enabled: boolean = false;
-                constructor(condition: boolean) {
-                    this.enabled = condition;
-                }
-
-                /** Clears the console if debugging is enabled */
-                    clear() {
-                        if (this.enabled) {
-                            console.clear();
-                        }
-                    }
-
-                /**
-                 * Create a heading within the browser console, if debugging is enabled
-                 * @param heading Text to display in the heading
-                 */
-                    heading(heading: string) {
-                        if (this.enabled) {
-                            console.log(`\n====================\n${heading}\n====================`);
-                        }
-                    }
-
-                /** Create a footer if debugging is enabled, allowing you to demark sections within the console */
-                    footer() {
-                        if (this.enabled) {
-                            console.log(`====================`);
-                        }
-                    }
-
-                /**
-                 * Write out the supplied args to the console, with tabbing
-                 * @param args Any items to output, separated by a comma, like for `console.log()`
-                 */
-                    log(...args: any[]) {
-                        if (this.enabled) {
-                            console.log('|\t', ...args);
-                        }
-                    }
-            }
-
-        /**
          * Gets property value for a particular object in a category.
          *
          * @function
