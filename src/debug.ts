92399bd737578f31f35f7236b24e7e56bd7f6f16
Daniel Marsh-Patrick <d.s.marsh@gmail.com>
2018-12-02 18:15:44 +1300

    #44: Added functionality to report time elasped since last report, if used multiple times within the same instance

--- src/debug.ts
+++ src/debug.ts
@@ -8,6 +8,7 @@ module powerbi.extensibility.visual {
         export class VisualDebugger {
             enabled: boolean = false;
             private startTime: number;
+            private lastCheckTime: number;
 
             constructor(condition: boolean) {
                 this.enabled = condition;
@@ -57,7 +58,11 @@ module powerbi.extensibility.visual {
                 reportExecutionTime() {
                     if (this.enabled) {
                         if (this.startTime) {
-                            this.log(`Total execution time: ${(performance.now() - this.startTime).toLocaleString()}ms`);
+                            if (this.lastCheckTime) {
+                                this.log(`${(performance.now() - this.lastCheckTime).toLocaleString()} ms elapsed since last report`);
+                            }
+                            this.log(`${(performance.now() - this.startTime).toLocaleString()} ms total execution time since profile start`);
+                            this.lastCheckTime = performance.now();
                         } else {
                             this.log('Unable to get execution time. Did you start profiling higher up in your code?');
                         }
