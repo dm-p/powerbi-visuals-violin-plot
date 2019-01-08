946cfb9e7acf4b6d0c9d808c0893039ecde890bd
Daniel Marsh-Patrick <d.s.marsh@gmail.com>
2019-01-08 15:02:25 +1300

    Updating metadata and settings for production/publication

--- src/settings.ts
+++ src/settings.ts
@@ -255,12 +255,12 @@ module powerbi.extensibility.visual {
     /** Used to hold about info and manage debugging */
     export class aboutSettings {
         public visualName: string = 'Violin Plot';
-        public version: string = '1.1.0.0';
+        public version: string = '1.1.0.2';
         public debugMode: boolean = false;
         public debugVisualUpdate: boolean = false;
         public debugTooltipEvents: boolean = false;
         public debugProperties: boolean = false;
-        public development: boolean = true; /** SET TO `false` FOR RELEASE */
+        public development: boolean = false; /** SET TO `false` FOR RELEASE */
         public usageUrl: string = 'https://bitbucket.org/dm-p/power-bi-visuals-violin-plot/wiki/Usage%20and%20Visual%20Properties';
     }
 
